d108ac9df2cac85283e6cd4911072a6711e23566
tim <timbeertoertchen@schademarmela.de>
2025-11-27 18:40:52 +0100

    Use MediaPlayer2 Interface to terminate Spotify

--- main.py
+++ main.py
@@ -6,41 +6,41 @@ from time import sleep
 SPOTIFY_URI = "org.mpris.MediaPlayer2.spotify"
 
 
+class MprisInterfaces:
+    def __init__(self, uri):
+        dbus_interface_info = {"dbus_uri": uri}
+        self.player = mpris2.Player(dbus_interface_info=dbus_interface_info)
+        self.mediaPlayer2 = mpris2.MediaPlayer2(
+            dbus_interface_info=dbus_interface_info
+        )
+
+    def is_ad(self) -> bool:
+        return not bool(self.player.CanGoNext)
+
+    def print_meta(self):
+        print(self.player.Metadata)
+
+
 def spawn_spotify() -> subprocess.Popen:
     spotify_process = subprocess.Popen("spotify")
     sleep(1)
     return spotify_process
 
 
-def get_spotify_player() -> mpris2.Player:
-    dbus_interface_info = {"dbus_uri": SPOTIFY_URI}
-    player = mpris2.Player(dbus_interface_info=dbus_interface_info)
-    return player
-
-
-def start_spotify() -> [subprocess.Popen, mpris2.Player]:
-    spotify_process = spawn_spotify()
-    player_interface = get_spotify_player()
-    player_interface.Play()
-    return spotify_process, player_interface
-
-
-def print_meta(player):
-    print(player.Metadata)
-
-
-def is_ad(player: mpris2.Player) -> bool:
-    return not bool(player.CanGoNext)
+def start_spotify() -> MprisInterfaces:
+    _ = spawn_spotify()
+    spotify_interfaces = MprisInterfaces(SPOTIFY_URI)
+    spotify_interfaces.player.Play()
+    return spotify_interfaces
 
 
 def watch_spotify():
-    spotify_process, player_interface = start_spotify()
+    spotify_interfaces = start_spotify()
     while True:
-        if is_ad(player_interface):
-            spotify_process.kill()
-            spotify_process = spawn_spotify()
-            player_interface = get_spotify_player()
-            player_interface.Play()
+        if spotify_interfaces.is_ad():
+            spotify_interfaces.mediaPlayer2.Quit()
+            sleep(1)
+            spotify_interfaces = start_spotify()
         else:
             sleep(3)
 
